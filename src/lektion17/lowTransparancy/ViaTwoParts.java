a73b0c0ac8b1ff6bd253bceab87920ec306aa6f2
AndersMikkelsen <ande077h@gmail.com>
2023-10-27 09:37:27 +0200

    distributed databases

--- src/lektion17/lowTransparancy/ViaTwoParts.java
+++ src/lektion17/lowTransparancy/ViaTwoParts.java
@@ -8,7 +8,7 @@ public class ViaTwoParts {
 // læser tabellen Personjyl via native-driver
         Connection minConnection;
         minConnection = DriverManager.getConnection("jdbc:sqlserver://localhost\\SQLEXPRESS;" +
-                "databaseName=ddbb;user=sa;password=123;");
+                "databaseName=ddba;user=sa;password=123;");
         String sql= "select * from personadr";
         Statement stmt = minConnection.createStatement();
         ResultSet res=stmt.executeQuery(sql);
@@ -22,17 +22,22 @@ public class ViaTwoParts {
 
 // l�ser tabellen Personoeer via native-driver
         Connection minCon2;
-        minCon2 = DriverManager.getConnection("jdbc:sqlserver://localhost;" +
+        minCon2 = DriverManager.getConnection("jdbc:sqlserver://localhost\\SQLEXPRESS;" +
                 "databaseName=ddbb;user=sa;password=123;");
         String sql2= "select * from personloen";
         Statement stmt2 = minCon2.createStatement();
         ResultSet res2=stmt2.executeQuery(sql2);
         while (res2.next()) {
-            Person p = new Person();
-            p.setCpr(res2.getString("cpr"));
-            p.setLoen(res2.getInt("loen"));
-            p.setSkatteprocent(res2.getInt("skatteprocent"));
-            liste.add(p);
+            for (Person p2 : liste) {
+                String cpr =  res2.getString("cpr");
+
+                if (p2.getCpr().equals(cpr)) {
+                    p2.setLoen(res2.getInt("loen"));
+                    p2.setSkatteprocent(res2.getInt("skatteprocent"));
+
+                }
+            }
+
         }
 
 // udskriver indholdet af de to tabeller
